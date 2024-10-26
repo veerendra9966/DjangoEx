b0fbe6d527efbd4d050634d92dbef775c67b6bb1
Veerendra Batchu <141005524+veerendra9966@users.noreply.github.com>
2024-10-26 14:24:35 +0530

    Update settings.py

--- sheryians_coding_school/settings.py
+++ sheryians_coding_school/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+wxz%$+2n6bw74##^-x_rx5e^yvo0y8i7yp6$15efsk)$ryc*=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
